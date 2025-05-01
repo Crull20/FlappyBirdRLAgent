737b8e017c8c6aa0fc5d43ceaf8e7acc5a5889d3
Andrepogi <108498217+Andrepogi@users.noreply.github.com>
2025-04-30 18:35:39 -0700

    Update memory.py with comments

--- memory.py
+++ memory.py
@@ -1,6 +1,7 @@
 from collections import deque
 import random
 
+# Class for storing past experiences. 
 class Memory():
     def __init__(self, maxlen, seed=None):
         self.memory = deque([], maxlen=maxlen)
@@ -8,12 +9,15 @@ class Memory():
         if seed is not None:
             random.seed(seed)
 
+    # Adds an experience to memory
     def add(self, experience):
         self.memory.append(experience)
 
+    # Returns a random sample of *batch_size* experiences from memory
     def sample(self, batch_size):
         return random.sample(self.memory, batch_size)
-    
+
+    # Returns # of stored experiences
     def __len__(self):
         return len(self.memory)
     
